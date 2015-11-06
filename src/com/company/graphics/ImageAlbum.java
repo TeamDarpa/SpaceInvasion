c4e6e6ebd66a0d6129fdccf6ed77d788ae7ad804
Elouel <elouel123@gmail.com>
2015-11-06 16:15:03 +0200

    libr
    librr

--- src/com/company/graphics/ImageAlbum.java
+++ src/com/company/graphics/ImageAlbum.java
@@ -2,11 +2,11 @@ package com.company.graphics;
 
 public enum ImageAlbum {
     //If you add another object to the game you must insert it in the enumeration here in order to get it's path
-    Player(""),
-    EasyEnemy(""),
-    SturdyEnemy(""),
+    Player("/resources/PlayerRebel.png"),
+    EasyEnemy("/resources/TIEFighter.png"),
+    SturdyEnemy("/resources/StarDestroyer.png"),
     DoubleDamageBonus(""),
-    Bullet("");
+    Bullet("/resources/bullet.png");
 
 
     private String path;
